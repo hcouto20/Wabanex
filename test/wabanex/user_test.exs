21f839e015fa229f2f818dbb6b7584c40c2ae6d8
Helder Alves <helder@brassun.com.br>
2021-06-27 19:52:48 -0300

    Aula 4

--- test/wabanex/user_test.exs
+++ test/wabanex/user_test.exs
@@ -1,9 +1,19 @@
 defmodule Wabanex.USerTest do
   use Wabanex.DataCase, async: true
 
-describe "changeset/1" do
-  test "when all params are valid, returns the user" do
-    params = %{name: "Helder", email: "heldercouto@live.com", password: "12345"}
+  alias Wabanex.User
+
+  describe "changeset/1" do
+    test "when all params are valid, returns a valid changeset " do
+      params = %{name: "Helder", email: "heldercouto@live.com", password: "123456"}
+
+      response = User.changeset(params)
+
+      assert %Ecto.Changeset{
+               valid?: true,
+               changes: %{email: "heldercouto@live.com", name: "Helder", password: "123456"},
+               errors: []
+             } = response
+    end
   end
-end
 end
